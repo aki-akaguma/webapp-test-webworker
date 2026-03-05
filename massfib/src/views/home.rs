70a2b70f2238bc1a7ce3a55f1cf7426ad3ef25be
aki <aki.akaguma@hotmail.com>
2026-03-05 20:46:56 +0900

    added: Single Worker

--- massfib/src/views/home.rs
+++ massfib/src/views/home.rs
@@ -1,5 +1,10 @@
 use dioxus::prelude::*;
 
+#[cfg(not(feature = "web"))]
+use std::time::Instant;
+#[cfg(feature = "web")]
+use web_time::Instant;
+
 const MAIN_JS: Asset = asset!("/assets/js/main.js", AssetOptions::js().with_minify(false));
 
 // This is necessary. Without it, `dx build --web --release` will not copy `worker.jp`.
@@ -12,86 +17,52 @@ const _WORKER_JS: Asset = asset!(
 
 #[component]
 pub fn Home() -> Element {
-    let a = use_signal(|| 2);
-    let b = use_signal(|| 3);
-    let mut add_r = use_signal(|| 0);
-    let mut fibo37_r = use_signal(|| 0);
-    let mut fibo38_r = use_signal(|| 0);
-    let mut fibo39_r = use_signal(|| 0);
-    let mut fibo40_r = use_signal(|| 0);
-    let mut fibo41_r = use_signal(|| 0);
+    let mut single_elapsed = use_signal(|| 0.0);
+    let mut multi_elapsed = use_signal(|| 0.0);
+    let add_r = use_signal(|| 0);
+    let fibo37_r = use_signal(|| 0);
+    let fibo38_r = use_signal(|| 0);
+    let fibo39_r = use_signal(|| 0);
+    let fibo40_r = use_signal(|| 0);
+    let fibo41_r = use_signal(|| 0);
 
     rsx! {
         document::Script { src: MAIN_JS }
-        div { "Hello world" }
+        div { " " }
         div {
             button {
                 onclick: move |_evt| async move {
-                    add_r.set(0);
-                    fibo37_r.set(0);
-                    fibo38_r.set(0);
-                    fibo39_r.set(0);
-                    fibo40_r.set(0);
-                    fibo41_r.set(0);
-                    //
-                    let js = r#"{ await kick_worker(dioxus); }"#;
-                    let mut eval = document::eval(js);
-                    dioxus_logger::tracing::debug!("PASS 001");
-                    //
-                    let arg = [1, *a.read(), *b.read()];
-                    dioxus_logger::tracing::debug!("send 1: {arg:?}");
-                    eval.send(arg).unwrap();
-                    //
-                    let arg = [2, 41, 0];
-                    dioxus_logger::tracing::debug!("send 2: {arg:?}");
-                    eval.send(arg).unwrap();
-                    let arg = [2, 40, 0];
-                    dioxus_logger::tracing::debug!("send 2: {arg:?}");
-                    eval.send(arg).unwrap();
-                    let arg = [2, 39, 0];
-                    dioxus_logger::tracing::debug!("send 2: {arg:?}");
-                    eval.send(arg).unwrap();
-                    let arg = [2, 38, 0];
-                    dioxus_logger::tracing::debug!("send 2: {arg:?}");
-                    eval.send(arg).unwrap();
-                    let arg = [2, 37, 0];
-                    dioxus_logger::tracing::debug!("send 2: {arg:?}");
-                    eval.send(arg).unwrap();
-                    //
-                    dioxus_logger::tracing::debug!("PASS 002");
-                    //
-                    for i in 0..6 {
-                        let result = eval.recv::<Vec<i32>>().await.unwrap();
-                        let sw = result[0];
-                        if sw == 1 {
-                            add_r.set(result[3]);
-                        } else if sw == 2 {
-                            let a = result[1];
-                            if a == 37 {
-                                fibo37_r.set(result[3]);
-                            } else if a == 38 {
-                                fibo38_r.set(result[3]);
-                            } else if a == 39 {
-                                fibo39_r.set(result[3]);
-                            } else if a == 40 {
-                                fibo40_r.set(result[3]);
-                            } else if a == 41 {
-                                fibo41_r.set(result[3]);
-                            }
-                        }
-                        dioxus_logger::tracing::debug!("PASS 003: {i}");
-                    }
-                    eval.send([9]).unwrap();
-                    dioxus_logger::tracing::debug!("PASS 004");
-                    let _ = eval.recv::<String>().await.unwrap();
-                    dioxus_logger::tracing::debug!("PASS 005");
+                    single_elapsed.set(0.0);
+                    let start = Instant::now();
+                    single_proc(add_r, fibo37_r, fibo38_r, fibo39_r, fibo40_r, fibo41_r).await;
+                    let elapsed = (start.elapsed().as_millis() as f32) / 1000.0;
+                    single_elapsed.set(elapsed);
                 },
-                "KICK WORKER"
+                "Single Worker"
+            }
+            span {
+                " {single_elapsed} sec"
             }
         }
         br{}
         div {
-            "{a} + {b} => {add_r}"
+            button {
+                onclick: move |_evt| async move {
+                    multi_elapsed.set(0.0);
+                    let start = Instant::now();
+                    multi_proc(add_r, fibo37_r, fibo38_r, fibo39_r, fibo40_r, fibo41_r).await;
+                    let elapsed = (start.elapsed().as_millis() as f32) / 1000.0;
+                    multi_elapsed.set(elapsed);
+                },
+                "Multi WORKER"
+            }
+            span {
+                " {multi_elapsed} sec"
+            }
+        }
+        br{}
+        div {
+            "2 + 3 => {add_r}"
         }
         br{}
         div { "fibonacci 37 => {fibo37_r}" }
@@ -100,4 +71,135 @@ pub fn Home() -> Element {
         div { "fibonacci 40 => {fibo40_r}" }
         div { "fibonacci 41 => {fibo41_r}" }
     }
+}
+
+async fn single_proc(
+    mut add_r: Signal<i32>,
+    mut fibo37_r: Signal<i32>,
+    mut fibo38_r: Signal<i32>,
+    mut fibo39_r: Signal<i32>,
+    mut fibo40_r: Signal<i32>,
+    mut fibo41_r: Signal<i32>,
+) {
+    add_r.set(0);
+    fibo37_r.set(0);
+    fibo38_r.set(0);
+    fibo39_r.set(0);
+    fibo40_r.set(0);
+    fibo41_r.set(0);
+    //
+    let js = r#"{ await kick_worker(dioxus); }"#;
+    let mut eval = document::eval(js);
+    dioxus_logger::tracing::debug!("PASS 001");
+    //
+    let arg = [1, 2, 3];
+    dioxus_logger::tracing::debug!("send 1: {arg:?}");
+    eval.send(arg).unwrap();
+    let result = eval.recv::<Vec<i32>>().await.unwrap();
+    add_r.set(result[3]);
+    //
+    let arg = [2, 37, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let result = eval.recv::<Vec<i32>>().await.unwrap();
+    fibo37_r.set(result[3]);
+    //
+    let arg = [2, 38, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let result = eval.recv::<Vec<i32>>().await.unwrap();
+    fibo38_r.set(result[3]);
+    //
+    let arg = [2, 39, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let result = eval.recv::<Vec<i32>>().await.unwrap();
+    fibo39_r.set(result[3]);
+    //
+    let arg = [2, 40, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let result = eval.recv::<Vec<i32>>().await.unwrap();
+    fibo40_r.set(result[3]);
+    //
+    let arg = [2, 41, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let result = eval.recv::<Vec<i32>>().await.unwrap();
+    fibo41_r.set(result[3]);
+    //
+    dioxus_logger::tracing::debug!("PASS 002");
+    //
+    eval.send([9]).unwrap();
+    dioxus_logger::tracing::debug!("PASS 004");
+    let _ = eval.recv::<String>().await.unwrap();
+    dioxus_logger::tracing::debug!("PASS 005");
+}
+
+async fn multi_proc(
+    mut add_r: Signal<i32>,
+    mut fibo37_r: Signal<i32>,
+    mut fibo38_r: Signal<i32>,
+    mut fibo39_r: Signal<i32>,
+    mut fibo40_r: Signal<i32>,
+    mut fibo41_r: Signal<i32>,
+) {
+    add_r.set(0);
+    fibo37_r.set(0);
+    fibo38_r.set(0);
+    fibo39_r.set(0);
+    fibo40_r.set(0);
+    fibo41_r.set(0);
+    //
+    let js = r#"{ await kick_worker(dioxus); }"#;
+    let mut eval = document::eval(js);
+    dioxus_logger::tracing::debug!("PASS 001");
+    //
+    let arg = [1, 2, 3];
+    dioxus_logger::tracing::debug!("send 1: {arg:?}");
+    eval.send(arg).unwrap();
+    //
+    let arg = [2, 41, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let arg = [2, 40, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let arg = [2, 39, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let arg = [2, 38, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    let arg = [2, 37, 0];
+    dioxus_logger::tracing::debug!("send 2: {arg:?}");
+    eval.send(arg).unwrap();
+    //
+    dioxus_logger::tracing::debug!("PASS 002");
+    //
+    for i in 0..6 {
+        let result = eval.recv::<Vec<i32>>().await.unwrap();
+        let sw = result[0];
+        if sw == 1 {
+            add_r.set(result[3]);
+        } else if sw == 2 {
+            let a = result[1];
+            if a == 37 {
+                fibo37_r.set(result[3]);
+            } else if a == 38 {
+                fibo38_r.set(result[3]);
+            } else if a == 39 {
+                fibo39_r.set(result[3]);
+            } else if a == 40 {
+                fibo40_r.set(result[3]);
+            } else if a == 41 {
+                fibo41_r.set(result[3]);
+            }
+        }
+        dioxus_logger::tracing::debug!("PASS 003: {i}");
+    }
+    eval.send([9]).unwrap();
+    dioxus_logger::tracing::debug!("PASS 004");
+    let _ = eval.recv::<String>().await.unwrap();
+    dioxus_logger::tracing::debug!("PASS 005");
 }
